78f9c693e5c6e9480646e715349845421336fac2
quambene <contact.quambene@gmail.com>
2024-12-03 02:47:58 +0100

    Add tests

--- tests/lib.rs
+++ tests/lib.rs
@@ -1,6 +1,9 @@
 extern crate readability;
+extern crate regex;
 extern crate url;
 
+use readability::{extractor, scorer::Scorer};
+use regex::Regex;
 use std::fs::File;
 use url::Url;
 
@@ -8,7 +11,7 @@ use url::Url;
 fn test_extract_title() {
     let mut file = File::open("./data/title.html").unwrap();
     let url = Url::parse("https://example.com").unwrap();
-    let product = readability::extractor::extract(&mut file, &url).unwrap();
+    let product = extractor::extract(&mut file, &url).unwrap();
     assert_eq!(product.title, "This is title");
 }
 
@@ -16,7 +19,7 @@ fn test_extract_title() {
 fn test_fix_rel_links() {
     let mut file = File::open("./data/rel.html").unwrap();
     let url = Url::parse("https://example.com").unwrap();
-    let product = readability::extractor::extract(&mut file, &url).unwrap();
+    let product = extractor::extract(&mut file, &url).unwrap();
     assert_eq!(product.content, "<!DOCTYPE html><html><head><title>This is title</title></head><body><p><a href=\"https://example.com/poop\"> poop </a></p></body></html>");
 }
 
@@ -24,6 +27,35 @@ fn test_fix_rel_links() {
 fn test_fix_img_links() {
     let mut file = File::open("./data/img.html").unwrap();
     let url = Url::parse("https://example.com").unwrap();
-    let product = readability::extractor::extract(&mut file, &url).unwrap();
+    let product = extractor::extract(&mut file, &url).unwrap();
     assert_eq!(product.content, "<!DOCTYPE html><html><head><title>This is title</title></head><body><p><img src=\"https://example.com/poop.png\"></p></body></html>");
+}
+
+#[test]
+fn test_comment() {
+    let mut file = File::open("./data/comment.html").unwrap();
+    let url = Url::parse("https://example.com").unwrap();
+    let product = extractor::extract(&mut file, &url).unwrap();
+    assert_eq!(
+        product.content,
+        "<!DOCTYPE html><html><head><title>This is title</title></head><body></body></html>"
+    );
+}
+
+#[test]
+fn test_comment_custom() {
+    let mut file = File::open("./data/comment.html").unwrap();
+    let url = Url::parse("https://example.com").unwrap();
+    let scorer = Scorer {
+        unlikely_candidates: &Regex::new(
+            "combx|community|disqus|extra|foot|header|menu|remark|rss|shoutbox|sidebar|sponsor|ad-break|agegate|pagination|pager|popup|tweet|twitter|ssba",
+        )
+        .unwrap(),
+        ..Default::default()
+    };
+    let product = extractor::extract_with_scorer(&mut file, &url, &scorer).unwrap();
+    assert_eq!(
+        product.content,
+        r#"<!DOCTYPE html><html><head><title>This is title</title></head><body><div class="comment">My comment with more than 20 characters.</div></body></html>"#
+    );
 }
