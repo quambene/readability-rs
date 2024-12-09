1e8359a479099e9c71f130a6b62a7031771963b3
quambene <contact.quambene@gmail.com>
2024-12-09 03:51:10 +0100

    Improve text formatting

--- src/dom.rs
+++ src/dom.rs
@@ -100,7 +100,10 @@ pub fn has_link(handle: Handle) -> bool {
     false
 }
 
+/// Convert HTML to formatted text, including linebreaks and whitespaces.
 pub fn extract_text(handle: Handle, text: &mut String, deep: bool) {
+    let mut last_tag_name = None;
+
     for child in handle.children.borrow().iter() {
         match child.data {
             Text { ref contents } => {
@@ -108,7 +111,17 @@ pub fn extract_text(handle: Handle, text: &mut String, deep: bool) {
             }
             Element { .. } => {
                 if deep {
+                    if let Some(tag_name) = last_tag_name {
+                        if &tag_name == "p" {
+                            text.push_str("\n");
+                        } else {
+                            text.push_str(" ");
+                        }
+                    }
+
                     extract_text(child.clone(), text, deep);
+
+                    last_tag_name = get_tag_name(child.clone());
                 }
             }
             _ => (),
