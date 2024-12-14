45dac2aba8cd2b8967321fa0a425451f9059197c
quambene <contact.quambene@gmail.com>
2024-12-14 03:28:17 +0100

    Refactor relative links

--- tests/lib.rs
+++ tests/lib.rs
@@ -100,30 +100,6 @@ fn test_extract_with_scorer(#[case] test_name: &str, #[case] url: &str) {
 }
 
 #[test]
-fn test_extract_title() {
-    let mut file = File::open("./data/title.html").unwrap();
-    let url = Url::parse("https://example.com").unwrap();
-    let product = extract(&mut file, &url).unwrap();
-    assert_eq!(product.title, "This is title");
-}
-
-#[test]
-fn test_fix_rel_links() {
-    let mut file = File::open("./data/rel.html").unwrap();
-    let url = Url::parse("https://example.com").unwrap();
-    let product = extract(&mut file, &url).unwrap();
-    assert_eq!(product.content, "<!DOCTYPE html><html><head><title>This is title</title></head><body><p><a href=\"https://example.com/poop\"> poop </a></p></body></html>");
-}
-
-#[test]
-fn test_fix_img_links() {
-    let mut file = File::open("./data/img.html").unwrap();
-    let url = Url::parse("https://example.com").unwrap();
-    let product = extract(&mut file, &url).unwrap();
-    assert_eq!(product.content, "<!DOCTYPE html><html><head><title>This is title</title></head><body><p><img src=\"https://example.com/poop.png\"></p></body></html>");
-}
-
-#[test]
 fn test_extract_malformed() {
     let html = r#"
         <!DOCTYPE html>
